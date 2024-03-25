c7b458d4618a0c0b9c8f0b155296ce51032a7155
syihwamoza <mozakastella26@gmail.com>
2024-03-25 10:17:07 +0700

    memasukkan fungsi perkalian

--- pemdas_25maret24/pemdas_25maret24.cpp
+++ pemdas_25maret24/pemdas_25maret24.cpp
@@ -12,10 +12,15 @@ void input()
 	cin >> b;
 }
 
-int penjumlahan(int bil_1, int bil_2) {
+int penjumlahan (int bil_1, int bil_2) {
 	return bil_1 + bil_2;
 }
 
 int pengurangan (int bil_1, int bil_2) {
 	return bil_1 - bil_2;
 }
+
+int perkalian (int bil_1, int bil_2) {
+	return bil_1 * bil_2;
+}
+
